7af069fe66b9e538bccf87d8841d4054972f2eff
Raghav <raghav@triffy.co>
2024-07-14 14:45:12 +0530

    "new changes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/sidebar'
+import Player from './components/Player/player';
+import Display from './components/Display/display';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar/>
+        <Display/>
+      </div>
+      <Player/>
+    </div>
   )
 }
 
